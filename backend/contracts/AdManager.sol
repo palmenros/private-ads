d45d599e3aad6c59c26c969504c1b59455c6fda6
Franco Sainas <franco@sainas.me>
2024-04-13 16:47:24 +0200

    feat: add AdManager tasks

--- backend/contracts/AdManager.sol
+++ backend/contracts/AdManager.sol
@@ -1,18 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
+
 contract AdManager {
 
   uint constant AD_PRICE = 1;
   uint constant FETCH_BATCH_SIZE = 300;
 
+  uint constant SALARY_WEIGHT = 1;
+  uint constant AGE_WEIGHT = 1000;
+
   struct AdData {
-    uint i;
+    uint age;
+    uint salary;
+    string url;
   }
 
   struct UserData {
+    uint age;
+    uint salary;
     bool isActive;
-    uint i;
   }
 
   struct Ad {
@@ -21,12 +29,12 @@ contract AdManager {
     uint showedSinceLastFetch;
   }
 
-  uint nextAdId = 0;
-  mapping (uint => Ad) ads;
-  uint[] activeAds;     // ids of active ads
-  mapping (address => UserData) users;
+  uint private nextAdId = 0;
+  mapping (uint => Ad) private ads;
+  uint[] private activeAds;     // ids of active ads
+  mapping (address => UserData) private users;
 
-  /* VIEW FUNCTIONS */
+  /****** VIEW FUNCTIONS ******/
   function _getNextAdId() public view returns (uint) {
     return nextAdId;
   }
@@ -39,6 +47,10 @@ contract AdManager {
     return users[addr];
   }
 
+  /**
+   * Checks if a user is malicious.
+   * @param user user to check
+   */
   function _isFraud(address user) private pure returns (bool) {
     /* TODO */
     return false;
@@ -48,19 +60,36 @@ contract AdManager {
     return AD_PRICE * amount;
   }
 
-  function _inference(AdData storage adData, UserData storage userData) view private returns (uint) {
-    return adData.i == userData.i ? 1 : 0;
+  /**
+   * Returns the absolute value of an int.
+   */
+  function _abs(int x) private pure returns (uint) {
+    return x >= 0 ? uint(x) : uint(-x);
+}
+
+  function _inference(AdData memory adData, UserData memory userData) pure public returns (uint) {
+    uint deltaAge = _abs(int(adData.age) - int(userData.age)) * AGE_WEIGHT;
+    uint deltaSalary = _abs(int(adData.salary) - int(userData.salary)) * SALARY_WEIGHT;
+    return deltaAge + deltaSalary;
   }
 
   /**
    * Register a new user.
+   * @param userData user data to register.
    */
   function register(UserData calldata userData) public {
+    console.log("Registering user...");
     require(!users[msg.sender].isActive, "You are already registered.");
     users[msg.sender] = userData;
     users[msg.sender].isActive = true;
+    console.log("User registered!");
   }
 
+  /**
+   * Post an ad to serve
+   * @param adData data of the da to post
+   * @param amountToShow number of ads to serve
+   */
   function postAd(AdData calldata adData, uint amountToShow) external payable {
 
     // Check that the amount payed to post the ad is correct
@@ -83,18 +112,20 @@ contract AdManager {
    * Serve the best ad to the user, the user gets the money in return.
    * The ad counter is decremented and if it reaches zero it is removed from active ads.
    */
-  function getAd() external returns (uint) {
+  function getAd() external returns (string memory) {
     require(users[msg.sender].isActive, "Inactive user.");
     require(!_isFraud(msg.sender), "Unauthorized user.");
 
+    console.log("Hello");
+
     // Find best ad
-    uint max = 0;
+    uint min = type(uint).max;
     uint bestAdId = 0;
     for (uint i = 0; i < activeAds.length; i++) {
 
       uint res = _inference(ads[activeAds[i]].data, users[msg.sender]);
-      if (max < res) {
-        max = res;
+      if (min > res) {
+        min = res;
         bestAdId = activeAds[i];
       }
       
@@ -122,13 +153,11 @@ contract AdManager {
     (bool success, ) = msg.sender.call{value: AD_PRICE}("");
     require(success);
 
-    return bestAdId;
+    return ads[bestAdId].data.url;
 
   }
 
   function getAdsLeft(uint id) public returns (uint) {
-    // TODO: implement fetching guard
-
     require(ads[id].showedSinceLastFetch >= FETCH_BATCH_SIZE, "Wait for the ad to be served enough times.");
     ads[id].showedSinceLastFetch = 0;
     return ads[id].adsLeft;
